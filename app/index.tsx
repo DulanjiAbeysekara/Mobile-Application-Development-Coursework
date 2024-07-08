3861ceb9842f12122c36d7b4c4ea74d30e2d437e
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-07-08 23:25:54 +0530

    creating logo and topic

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,51 @@
-import { View, Text,StyleSheet} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native';
+import React from 'react';
 
-export default function index() {
+export default function Index() {
   return (
     <View style={styles.backgroundView}>
-      <Text>Million of songs.
-      Free on Spotify.
+
+      <Text style={styles.text}>
+        Million of songs.
+       
       </Text>
+
+      <Text style={styles.text1}>
+        Free on Spotify.
+      </Text> 
+
+      <Image
+        style={styles.spotifyLogo}
+        source={require('../assets/images/Spotify_Primary_Logo_RGB_White.png')}
+      />
     </View>
-  )
+  );
 }
 
-const styles= StyleSheet.create({
-  backgroundView:{
-    flex:1,
-    alignItems:'center',
-    margin:10,
-    backgroundColor:'black'
+const styles = StyleSheet.create({
+  backgroundView: {
+    flex: 1,
+    alignItems: 'center', 
+    margin: 10,
+    backgroundColor: 'black',
+  },
+  text: {
+    color: 'white',  
+    textAlign: 'center',  
+    marginBottom: 20, 
+    fontSize:30
+
+  },
+
+  text1: {
+    color: 'white',  
+    textAlign: 'center',  
+    marginBottom: 20, 
+    fontSize:30,
+    marginTop:-20
+  },
+  spotifyLogo: {
+    width: 50,
+    height: 50
   }
-})
+});
