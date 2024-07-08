3bc138dcb21557b39f1795cb2504505365c29738
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-07-08 22:20:11 +0530

    creating background color

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet} from 'react-native'
 import React from 'react'
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.backgroundView}>
+      <Text>Million of songs.
+      Free on Spotify.
+      </Text>
     </View>
   )
 }
+
+const styles= StyleSheet.create({
+  backgroundView:{
+    flex:1,
+    alignItems:'center',
+    margin:10,
+    backgroundColor:'black'
+  }
+})
