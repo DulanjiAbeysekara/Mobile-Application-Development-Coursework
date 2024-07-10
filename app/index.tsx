cce60413386af00d28c38d34a84630d46bd02cd9
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-07-10 21:19:25 +0530

    created creating page

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,29 @@
-import { View, Text, StyleSheet, TextInput,TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState,useEffect  } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'; // Import DateTimePickerEvent
 
 export default function Index() {
   return (
     <View style={styles.backgroundView2}>
-
       <Text style={styles.topic2}>Create account</Text>
 
       <Text style={styles.topic}>What's your date of birth?</Text>
 
-      <TextInput style={styles.inputFeild} placeholder="MM/DD/YYYY" placeholderTextColor="gray" />
+      <TextInput style={styles.inputFeild} placeholder="MM/DD/YYYY" placeholderTextColor="white"/>
 
-      <TouchableOpacity style={styles.nextBtn1} >
+
+      <TouchableOpacity style={styles.nextBtn1}>
         <Text style={styles.nextBtnText}>Next</Text>
       </TouchableOpacity>
+    
     </View>
-  )
+  ); 
 }
 
 const styles = StyleSheet.create({
   backgroundView2: {
     flex: 1,
-    alignItems: 'flex-start', 
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
     margin: 10,
     backgroundColor: 'black',
@@ -30,46 +32,44 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: 'white',
     textAlign: 'center',
-    fontWeight: '600', 
-    top: 5, 
+    fontWeight: '600',
+    top: 5,
     left: 140,
   },
   topic: {
     fontSize: 20,
     color: 'white',
     textAlign: 'left',
-    fontWeight: '600', 
-    marginBottom: 25, 
-    left: 20, 
-    top: 30,  
+    fontWeight: '600',
+    marginBottom: 25,
+    left: 15,
+    top: 30,
   },
   inputFeild: {
     height: 40,
-    width: '90%', 
+    width: '90%',
     borderColor: '#848884',
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     color: 'white',
     marginTop: 10,
-    alignSelf: 'center', 
+    alignSelf: 'center',
     backgroundColor: '#848884',
   },
-  nextBtn1:{
+  nextBtn1: {
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 50,
     width: 75,
     alignItems: 'center',
     marginTop: 85,
-    backgroundColor:'white',
+    backgroundColor: 'white',
     fontWeight: 'bold',
-    left:160,
-    
+    left: 140,
   },
-  nextBtnText:{
-    fontWeight: 'bold', 
-    color: 'black', 
-  }
-  
-})
+  nextBtnText: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
